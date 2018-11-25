48a7512aaa7bf795637bb6fb3f51c0560a7b1f25
Adam Guinea <adamguinea22@gmail.com>
2018-11-25 23:26:05 +0000

    added iframe to results page

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,36 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../APIs/Youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-    state = {videos: [] };
+    state = {videos: [], selectedVideo: null };
+    
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
-        })
+        });
         
-       this.setState({videos: response.data.items}) 
+       this.setState({videos: response.data.items}) ;
     }
+    
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    };
     
     render(){
         return (
             <div>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} /> 
+                <VideoList 
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos} 
+                />
             </div>
-        )
+        );
     }
 }
 
