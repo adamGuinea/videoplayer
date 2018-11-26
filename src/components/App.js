e9b34a56a1439e4d9c847df0040ee91a3097931c
Adam Guinea <adamguinea22@gmail.com>
2018-11-26 01:35:38 +0000

    added responsiveness to page

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = {videos: [], selectedVideo: null };
     
+    componentDidMount(){
+        this.onTermSubmit('React.js')
+    }
+    
     onTermSubmit = async (term) => {
         const response = await youtube.get('/search', {
             params: {
@@ -14,10 +18,13 @@ class App extends React.Component {
             }
         });
         
-       this.setState({videos: response.data.items}) ;
-    }
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        }) ;
+    };
     
-    onVideoSelect = (video) => {
+    onVideoSelect = video => {
         this.setState({selectedVideo: video});
     };
     
@@ -25,11 +32,19 @@ class App extends React.Component {
         return (
             <div>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} /> 
-                <VideoList 
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos} 
-                />
+                <div className='ui stackable grid container'>
+                    <div className='ui row'>
+                        <div className='eleven wide column'>
+                            <VideoDetail video={this.state.selectedVideo} /> 
+                        </div>
+                        <div className='five wide column'>
+                            <VideoList 
+                            onVideoSelect={this.onVideoSelect}
+                            videos={this.state.videos} 
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,8 +9,10 @@ const VideoDetail = ({video}) => {
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     
     return (
-        <div className='ui embed'>
-            <iframe title='video player' src={videoSrc} />
+        <div>
+            <div className='ui embed'>
+                <iframe title='video player' src={videoSrc} />
+            </div>
             <div className='ui segment'>
                 <h4 className='ui header'>{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
